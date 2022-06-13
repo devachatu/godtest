0f26ac996829399161912aa8b12b7d81774162ce
ChaitanyaApplore <chaitanya@applore.in>
2022-06-14 00:15:28 +0530

    First Commit

---
+++ src/main/java/com/god/godtest/GodTestApplication.java
@@ -0,0 +1,13 @@
+package com.god.godtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GodTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GodTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/god/godtest/GodTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.god.godtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GodTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
